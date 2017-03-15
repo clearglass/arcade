db9d02e0ff021648c68d05d8f89fb6fe8ae3e6f5
clearglass <clearglass@users.noreply.github.com>
2017-03-14 18:30:10 -0700

    Update circle_example.py

--- src/geometric/circle_example.py
+++ src/geometric/circle_example.py
@@ -1,21 +1,21 @@
 import simplegui, time, random
 
-WINDOW_WIDTH = 500
-WINDOW_HEIGHT = WINDOW_WIDTH
-GLOBAL_CIRCLE_DIAMETER = 25
+class Constants:
+    WINDOW_WIDTH = 500
+    WINDOW_HEIGHT = WINDOW_WIDTH
+    GLOBAL_CIRCLE_DIAMETER = 25
 
-GRID_WIDTH = WINDOW_WIDTH / 25
-GRID_HEIGHT = GRID_WIDTH
-
-DEFAULT_CIRCLE_DRAW_ATTRIBUTES = {
-    "line_width": 2,
-    "line_color": 'Blue',
-    "fill_color": 'Aqua'
-}
-
+    GRID_WIDTH = WINDOW_WIDTH / 25
+    GRID_HEIGHT = GRID_WIDTH
 class Circle:
 
-    DIAMETER = GLOBAL_CIRCLE_DIAMETER
+    DEFAULT_CIRCLE_DRAW_ATTRIBUTES = {
+        "line_width": 2,
+        "line_color": 'Blue',
+        "fill_color": 'Aqua'
+    }    
+    
+    DIAMETER = Constants.GLOBAL_CIRCLE_DIAMETER
     RADIUS = DIAMETER / 2
     SHAPE_ATTRIBUTES = DEFAULT_CIRCLE_DRAW_ATTRIBUTES
 
@@ -34,16 +34,21 @@ class Circle:
         )
 
 circ_list = []
-for i in range(GRID_WIDTH):
-    for j in range(GRID_WIDTH):
+for i in range(Constants.GRID_WIDTH):
+    for j in range(Constants.GRID_WIDTH):
         circ_list.append(Circle((i,j)))
                                
 def draw(canvas):
     for circ in circ_list:
         circ.draw_me(canvas)
-        
-frame = simplegui.create_frame("Home", WINDOW_WIDTH, WINDOW_HEIGHT)
-frame.set_canvas_background("Silver")
-frame.set_draw_handler(draw)
-frame.start()        
-              
+
+class Graphics:
+    WINDOW_WIDTH = Constants.WINDOW_WIDTH
+    WINDOW_HEIGHT = Constants.WINDOW_HEIGHT
+    
+    def __init__(self, width=WINDOW_WIDTH, height=WINDOW_HEIGHT):
+        frame = simplegui.create_frame("Home", width, height)
+        frame.set_canvas_background("Silver")
+        frame.set_draw_handler(draw)
+        frame.start()                     
+Graphics()
